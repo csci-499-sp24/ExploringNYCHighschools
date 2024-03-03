de6c3fa3a21b227be358f0e0fb32096775f013ea
Tammy Chiu <chiutammy182@gmail.com>
2024-03-03 17:35:38 -0500

    hide external database url.

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ require('dotenv').config() // to use .env file
 const { Sequelize } = require("sequelize");
 
 // Instantiate Sequelize instance with database external URL from Render
-// When pushing to github, use DB_URL_INTERNAL but for local use DB_URL
+// When pushing to github, use DB_URL_INTERNAL but for local use external url(get from Render website)
 const db = new Sequelize(process.env.DB_URL_INTERNAL, {
     dialect: "postgres",
     // logging: false,

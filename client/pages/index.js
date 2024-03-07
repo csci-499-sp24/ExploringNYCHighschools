179e684ee786be3dc236b1f8fc9ed31b6465ca30
kahleongkhoo <56051868+kahleongkhoo@users.noreply.github.com>
2024-03-07 15:02:07 -0500

    Update index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,27 +1,35 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 function Index() {
-  
-  const [message, setMessage] = useState("Loading")
+  const [schools, setSchools] = useState([]);
+  const [message, setMessage] = useState("Loading");
 
-  console.log(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home")
+  console.log(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home");
   useEffect(() => {
-    fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home").then(
-      response => response.json()
-    ).then(
-      data => {
-        console.log(data)
-        setMessage(data.message)
-      }
-    )
-  }, [])
+    fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home")
+      .then((response) => response.json())
+      .then((data) => {
+        setSchools(data.schools);
+        setMessage(data.message);
+      });
+  }, []);
 
   return (
     <div>
-      <div>Return message from server</div>
-      <div>{message}</div>
+      <h1>Schools:</h1>
+      {message ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {schools.map((school, index) => (
+            <li key={index}>
+              <strong>{school.school_name}</strong> <br /> {school.address}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
+  );
 }
 
-export default Index
+export default Index;

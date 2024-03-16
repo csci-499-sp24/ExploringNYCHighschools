a9902ca2e8631827125f24176923762f7413ce04
Tammy Chiu <chiutammy182@gmail.com>
2024-03-16 16:59:56 -0400

    Layout for school profile.

---
+++ client/pages/school.js
@@ -0,0 +1,139 @@
+function SchoolPage() {
+    return (
+        <section id="hero">
+            <div className="container">
+                <div className="row">
+                    <h1 className="display-1">School Name</h1>
+                    <p className="desc">School Description</p>
+                </div>
+            </div>
+                <div className="row">
+                    <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
+                        <div className="p-2 border flex-fill bd-highlight">
+                            <div className="card-fill">
+                                <div className="card-body">
+                                    <h6 className="card-title">Address: </h6>
+                                    <h6 className="card-title">Website: </h6>
+                                    <h6 className="card-title">Phone number: </h6>
+                                    <h6 className="card-title">Email: </h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Languages</h5>
+                                    <p className="card-text">List of languages offered at this school.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">AP Cources</h5>
+                                    <p className="card-text">List of AP courses offered at this school.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Graduation Rate</h5>
+                                    <p className="card-text">Insert graduation rate of school.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>     
+                </div>
+                <div className="row">
+                    <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Subway</h5>
+                                <p className="card-text">Trains that students can take that arrive to school location.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Buses</h5>
+                                <p className="card-text">Buses that students can take that arrive to school location.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Grade Span</h5>
+                                <p className="card-text">What grades are taught at this school.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>     
+                </div>
+                <div className="row">
+                    <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Total Students</h5>
+                                <p className="card-text">Total number of students attending this school.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Freshman Schedule</h5>
+                                <p className="card-text">Insert arrival and dismissal time for freshman.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">PSAL Boys</h5>
+                                <p className="card-text">List of sports offered for boys at this school.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>     
+                </div>
+                <div className="row">
+                    <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">PSAL Girls</h5>
+                                <p className="card-text">List of sports offered for girls at this school.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Attendance Rate</h5>
+                                <p className="card-text">Insert attendance rate.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-2 bd-highlight">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">School Safety</h5>
+                                <p className="card-text">Rating of school safety.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>     
+                </div>
+        </section>
+    )
+}
+export default SchoolPage

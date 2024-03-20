7fd150d8614f0037de3fd86d155db217472f0b1e
Tammy Chiu <chiutammy182@gmail.com>
2024-03-20 16:12:09 -0400

    Added key to map function.

--- client/pages/schools.js
+++ client/pages/schools.js
@@ -18,13 +18,16 @@ function Schools() {
         <section id="hero">
             <h1 className="display-1">Explore High Schools</h1>
                 <div>
-                    {schools.map((school) => (
-                        <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
-                        <div className="p-2 border flex-fill bd-highlight">
-                            <Card text1={`Address: ${school.school_name}`} text2={`Website: ${school.website}`} text3={`Phone Number: ${school.phone_number}`} text4={`Email: ${school.email}`}></Card>
-                            <div className="school-button">
+                    {schools.map((school,index) => (
+                        <div key={index}>
+                            <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
+                                <div className="p-2 border flex-fill bd-highlight">
+                                    <Card text1={`Address: ${school.school_name}`} text2={`Website: ${school.website}`} text3={`Phone Number: ${school.phone_number}`} text4={`Email: ${school.email}`}></Card>
+                                
+                                <div className="school-button">
                                     <SchoolButton link={`http://localhost:3000/schools/${school.dbn}`}></SchoolButton>
                                 </div>
+                            </div>
                         </div>
                     </div>
                     ))}

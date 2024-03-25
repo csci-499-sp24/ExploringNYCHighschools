4604d7e65c46adbd0fab7b4e0ede65f203e99800
kahleongkhoo <56051868+kahleongkhoo@users.noreply.github.com>
2024-03-25 16:31:07 -0400

    Update schools.js
    added deboucing

--- client/pages/schools.js
+++ client/pages/schools.js
@@ -1,124 +1,134 @@
-import { useEffect, useState } from "react";
-import SchoolButton from "../components/SchoolButton";
-import Card from "../components/Card";
-import ScrollUpButton from "../components/ScrollUpButton";
+import React, { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
+import Card from "../components/Card";
+import SchoolButton from "../components/SchoolButton";
+import ScrollUpButton from "../components/ScrollUpButton";
 
 function Schools() {
-    const [schools, setSchools] = useState([]);
-    const [message, setMessage] = useState("Loading");
+  const [schools, setSchools] = useState([]);
+  const [message, setMessage] = useState("Loading");
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState(null);
+  const [debouncedQuery, setDebouncedQuery] = useState("");
+  const [debouncedSearchType, setDebouncedSearchType] = useState("school");
+  const [timeoutId, setTimeoutId] = useState(null);
+  const [showNoMatchesMessage, setShowNoMatchesMessage] = useState(false);
 
-    useEffect(() => {
-        fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/schools")
-        .then((response) => response.json())
-        .then((data) => {
-            setSchools(data.schools);
-            setMessage(data.message);
-        });
-    }, [schools]);
+  useEffect(() => {
+    fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/schools")
+      .then((response) => response.json())
+      .then((data) => {
+        setSchools(data.schools);
+        setMessage(data.message);
+      });
+  }, []);
 
-    const [results, setResults] = useState([]);
-    const [error, setError] = useState(null);
-    const [foundQuery, setFoundQuery] = useState(false);
-  
-    const handleSearch = async (query, searchType) => {
-      try {
-        if (!query) {
-          // If query is empty, reset results and return
-          setResults([]);
-          setFoundQuery(false);
-          return;
-        }
-  
-        let endpoint = "";
-        if (searchType === "neighborhood") {
-          endpoint = `http://localhost:8080/api/schools?neighborhood=${query}`;
-        } else if (searchType === "school") {
-          endpoint = `http://localhost:8080/api/schools?q=${query}`;
-        } else if (searchType === "languages") {
-          endpoint = `http://localhost:8080/api/schools?languages=${query}`;
-        } else if (searchType === "ap_classes") {
-          endpoint = `http://localhost:8080/api/schools?ap_classes=${query}`;
-        }
-  
-        const res = await fetch(endpoint);
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await res.json();
-  
-        // Assuming "schools" is the array of schools in the response
-        const schools = data.schools;
-  
-        // Filter schools based on the query and search type
-        const filteredSchools = schools.filter((school) => {
-          if (searchType === "neighborhood") {
-            return school.neighborhood
-              .toLowerCase()
-              .includes(query.toLowerCase());
-          } else if (searchType === "school") {
-            return school.school_name.toLowerCase().includes(query.toLowerCase());
-          } else if (searchType === "languages") {
-            return (
-              school.languages &&
-              school.languages.toLowerCase().includes(query.toLowerCase())
-            );
-          } else if (searchType === "ap_classes") {
-            return (
-              school.ap_classes &&
-              school.ap_classes.toLowerCase().includes(query.toLowerCase())
-            );
-          }
-        });
-  
-        // Set the schools array as the state
-        setResults(filteredSchools);
-        setFoundQuery(true);
-        setError(null);
-      } catch (error) {
-        setError("An error occurred while fetching search results");
+  useEffect(() => {
+    const delayTimer = setTimeout(() => {
+      if (debouncedQuery.trim() !== "") {
+        handleSearch(debouncedQuery, debouncedSearchType);
+        setShowNoMatchesMessage(true);
+      } else {
         setResults([]);
-        setFoundQuery(false);
+        setShowNoMatchesMessage(false);
       }
-    };
-    return (
-        <div className="background-color">
-        <ScrollUpButton/>
-        <section id="hero">
-            <h1 className="display-1">Explore High Schools</h1>
-            <SearchBar onSearch={handleSearch} />
-                {error && <p>{error}</p>}
-                <div>
-                {foundQuery ? (
-                    results.length > 0 ? (results.map((school, index) => (
-                            <div key={index} className="d-flex flex-row bd-highlight mb-3 justify-content-center">
-                                <div className="p-2 border flex-fill bd-highlight">
-                                    <Card text1={`School Name: ${school.school_name}`} text2={`Address: ${school.address}`} text3={`Website: ${school.website}`} text4={`Phone Number: ${school.phone_number}`} text5={`Email: ${school.email}`}></Card>
-                                
-                                <div className="school-button">
-                                    <SchoolButton link={`/schools/${school.dbn}`}></SchoolButton>
-                                </div>
-                            </div>
-                        </div>
-                ))
-                ) : ( <p className="search">No matches found!</p> )
-                ) : (schools.map((school, index) => (
-                    <div key={index} className="d-flex flex-row bd-highlight mb-3 justify-content-center">
-                                <div className="p-2 border flex-fill bd-highlight">
-                                    <Card text1={`School Name: ${school.school_name}`} text2={`Address: ${school.address}`} text3={`Website: ${school.website}`} text4={`Phone Number: ${school.phone_number}`} text5={`Email: ${school.email}`}></Card>
-                                
-                                <div className="school-button">
-                                    <SchoolButton link={`/schools/${school.dbn}`}></SchoolButton>
-                                </div>
-                            </div>
-                        </div>
-                ))
-                )}
+    }, 500);
+
+    setTimeoutId(delayTimer);
+
+    return () => clearTimeout(delayTimer);
+  }, [debouncedQuery, debouncedSearchType]);
+
+  const handleSearch = async (query, searchType) => {
+    try {
+      let endpoint = "";
+      if (searchType === "neighborhood") {
+        endpoint = `http://localhost:8080/api/schools?neighborhood=${query}`;
+      } else if (searchType === "school") {
+        endpoint = `http://localhost:8080/api/schools?q=${query}`;
+      } else if (searchType === "languages") {
+        endpoint = `http://localhost:8080/api/schools?languages=${query}`;
+      } else if (searchType === "ap_classes") {
+        endpoint = `http://localhost:8080/api/schools?ap_classes=${query}`;
+      }
+
+      const res = await fetch(endpoint);
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await res.json();
+      const schools = data.schools;
+      const filteredSchools = schools.filter((school) => {
+        if (searchType === "neighborhood") {
+          return school.neighborhood
+            .toLowerCase()
+            .includes(query.toLowerCase());
+        } else if (searchType === "school") {
+          return school.school_name.toLowerCase().includes(query.toLowerCase());
+        } else if (searchType === "languages") {
+          return (
+            school.languages &&
+            school.languages.toLowerCase().includes(query.toLowerCase())
+          );
+        } else if (searchType === "ap_classes") {
+          return (
+            school.ap_classes &&
+            school.ap_classes.toLowerCase().includes(query.toLowerCase())
+          );
+        }
+      });
+
+      setResults(filteredSchools);
+      setError(null);
+    } catch (error) {
+      setError("An error occurred while fetching search results");
+      setResults([]);
+    }
+  };
+
+  return (
+    <div className="background-color">
+      <ScrollUpButton />
+      <section id="hero">
+        <h1 className="display-1">Explore High Schools</h1>
+        <SearchBar
+          onSearch={(query, searchType) => {
+            setDebouncedQuery(query);
+            setDebouncedSearchType(searchType);
+          }}
+        />
+        {error && <p>{error}</p>}
+        <div>
+          {results.length > 0 ? (
+            results.map((school, index) => (
+              <div
+                key={index}
+                className="d-flex flex-row bd-highlight mb-3 justify-content-center"
+              >
+                <div className="p-2 border flex-fill bd-highlight">
+                  <Card
+                    text1={`School Name: ${school.school_name}`}
+                    text2={`Address: ${school.address}`}
+                    text3={`Website: ${school.website}`}
+                    text4={`Phone Number: ${school.phone_number}`}
+                    text5={`Email: ${school.email}`}
+                  ></Card>
+
+                  <div className="school-button">
+                    <SchoolButton
+                      link={`/schools/${school.dbn}`}
+                    ></SchoolButton>
+                  </div>
                 </div>
-                
-        </section>
+              </div>
+            ))
+          ) : showNoMatchesMessage ? (
+            <p className="search">No matches found!</p>
+          ) : null}
         </div>
-    )            
+      </section>
+    </div>
+  );
 }
 
 export default Schools;

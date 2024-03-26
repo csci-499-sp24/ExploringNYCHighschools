d2d7f6438a8bb0be63db985164374f3ec27a443a
orijinel <44485040+orijinel@users.noreply.github.com>
2024-03-26 17:28:37 -0400

    Update index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,27 +1,26 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
+import App from './homepage';
 
 function Index() {
-  
-  const [message, setMessage] = useState("Loading")
+  const [message, setMessage] = useState("Loading");
 
-  console.log(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home")
+  console.log(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home");
   useEffect(() => {
-    fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home").then(
-      response => response.json()
-    ).then(
-      data => {
-        console.log(data)
-        setMessage(data.message)
-      }
-    )
-  }, [])
+    fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/home")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setMessage(data.message);
+      });
+  }, []);
 
   return (
     <div>
       <div>Return message from server</div>
       <div>{message}</div>
+      <App prop1="Value 1" prop2="Value 2" />
     </div>
-  )
+  );
 }
 
-export default Index
+export default Index;

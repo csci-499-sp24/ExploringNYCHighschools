c1dd7b37f3e0feafda4d6389bec20d20a67d85ca
Tammy Chiu <chiutammy182@gmail.com>
2024-03-30 23:01:49 -0400

    Use external url

--- server/db.js
+++ server/db.js
@@ -3,8 +3,7 @@ const { Sequelize } = require("sequelize");
 const pg = require('pg');
 
 // Instantiate Sequelize instance with database external URL from Render
-// When pushing to github, use DB_URL_INTERNAL but for local use external url(get from Render website)
-const db = new Sequelize(process.env.DB_URL_INTERNAL, {
+const db = new Sequelize(process.env.DB_URL, {
     dialect: "postgres",
     dialectModule: pg,
     logging: false,

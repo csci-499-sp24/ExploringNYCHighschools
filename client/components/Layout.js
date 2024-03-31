d93fb73ad8982ff59c8242a4c2148f76198492f1
orijinel <44485040+orijinel@users.noreply.github.com>
2024-03-31 14:44:31 -0400

    Update Layout.js
    added navbar and background to layout for styling consistency

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ScrollUpButton from "./ScrollUpButton";
+import NavBar from "./NavBar";
+import Background from "./Background";
+import { propTypes } from "react-bootstrap/esm/Image";
 
 const Layout = ({props}) => {
     return(

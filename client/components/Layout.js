16f31fb4299d6ad7011b358a6e2ad2a49974a3ed
orijinel <44485040+orijinel@users.noreply.github.com>
2024-03-31 14:52:18 -0400

    Update Layout.js

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ScrollUpButton from "./ScrollUpButton";
-import NavBar from "./NavBar";
-import Background from "./Background";
-import { propTypes } from "react-bootstrap/esm/Image";
 
 const Layout = ({props}) => {
     return(

687131f015c7ce4db7a14f1b7a9e8a5f51fddb67
kahleongkhoo <56051868+kahleongkhoo@users.noreply.github.com>
2024-04-01 15:48:05 -0400

    Update SearchBar.js

--- client/components/SearchBar.js
+++ client/components/SearchBar.js
@@ -2,25 +2,23 @@ import React, { useState, useEffect } from "react";
 
 const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState("");
-  const [searchType, setSearchType] = useState("school");
+  const [prevQuery, setPrevQuery] = useState("");
 
   useEffect(() => {
     const delayTimer = setTimeout(() => {
       // Trigger the search after a delay of 500 milliseconds
-      onSearch(query, searchType);
+      if (query !== prevQuery) {
+        onSearch(query);
+        setPrevQuery(query);
+      }
     }, 500);
 
     return () => clearTimeout(delayTimer); // Clear the timer on component unmount or query/searchType change
-  }, [query, searchType, onSearch]);
+  }, [query, onSearch, prevQuery]);
 
   const handleChange = (e) => {
     const searchTerm = e.target.value;
     setQuery(searchTerm);
-  };
-
-  const handleSearchTypeChange = (e) => {
-    const newSearchType = e.target.value;
-    setSearchType(newSearchType);
   };
 
   return (
@@ -29,31 +27,14 @@ const SearchBar = ({ onSearch }) => {
         <input
           type="text"
           className="w-full py-2 px-4 outline-none rounded-l-md border border-blue-500 mr-2"
-          placeholder={`Search by ${
-            searchType === "school"
-              ? "school name"
-              : searchType === "neighborhood"
-              ? "neighborhood"
-              : searchType === "languages"
-              ? "languages"
-              : "AP classes"
-          }...`}
+          placeholder={`Search by school...`}
           value={query}
           onChange={handleChange}
         />
-        <select
-          className="py-2 px-4 outline-none border border-blue-500 rounded-r-md"
-          value={searchType}
-          onChange={handleSearchTypeChange}
-        >
-          <option value="school">School Name</option>
-          <option value="neighborhood">Neighborhood</option>
-          <option value="languages">Languages</option>
-          <option value="ap_classes">AP Classes</option>
-        </select>
       </div>
     </div>
   );
 };
 
 export default SearchBar;
+

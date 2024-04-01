59a1219bc1556c669fa44b0b7460c72ce5433362
Tammy Chiu <chiutammy182@gmail.com>
2024-03-31 22:31:22 -0400

    updated test-coverage

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
-// require('dotenv').config() // to use .env file
+require('dotenv').config() // to use .env file
 const { Sequelize } = require("sequelize");
 const pg = require('pg');
 
 // Instantiate Sequelize instance with database external URL from Render
 // When pushing to github, use DB_URL_INTERNAL but for local use external url(get from Render website)
-const db = new Sequelize(process.env.DB_URL_INTERNAL, {
+const db = new Sequelize(process.env.DB_URL, {
     dialect: "postgres",
     dialectModule: pg,
     logging: false,

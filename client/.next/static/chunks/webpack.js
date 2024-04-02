39f42fa31dc5b16e6919e1800edff9aefbdf9403
unknown <dulinagunasekara@gmail.com>
2024-04-02 13:46:29 -0400

    added contact route

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f252a18e129c5acc"; }
+/******/ 		__webpack_require__.h = function() { return "f572cd471e6867de"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

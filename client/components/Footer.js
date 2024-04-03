1431728fb36e72c9079e133718bb4fd22bfe6eef
Connie Qiu <connieqiu24@gmail.com>
2024-04-03 19:23:11 -0400

    Added social media icons and spacing styling to make it look nice

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from '../styles/Footer.module.css';
+import { BsInstagram, BsTwitter, BsGithub, BsFacebook } from "react-icons/bs";
 
 const Footer = () => {
     return (
@@ -8,6 +9,21 @@ const Footer = () => {
                 <p id={styles.about}>About Us</p>
                 <p id={styles.faq}>FAQ</p>
                 <p id={styles.contactUs}>Contact Us</p>
+                <p id={styles.feedback}>Site Feedback</p>
+            </div>
+            <div className={styles.iconsDiv}>
+                <div className={styles.spacerForIcons}>
+                    <BsInstagram/>
+                </div>
+                <div className={styles.spacerForIcons}>
+                    <BsTwitter/>
+                </div>
+                <div className={styles.spacerForIcons}>
+                    <BsGithub/>
+                </div>
+                <div className={styles.spacerForIcons}>
+                    <BsFacebook/>
+                </div>
             </div>
         </div>
     )

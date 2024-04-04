7c0ea7f64e415191ed630eb7222361f956144b4d
Connie Qiu <connieqiu24@gmail.com>
2024-04-04 17:43:49 -0400

    Changed the text and icons to be buttons, linked all of them to the homepage for now, added CSS

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'next/link';
 import styles from '../styles/Footer.module.css';
 import { BsInstagram, BsTwitter, BsGithub, BsFacebook } from "react-icons/bs";
 
@@ -6,23 +7,55 @@ const Footer = () => {
     return (
         <div className={styles.footer}>
             <div className={styles.aboutFaqContact}>
-                <p id={styles.about}>About Us</p>
-                <p id={styles.faq}>FAQ</p>
-                <p id={styles.contactUs}>Contact Us</p>
-                <p id={styles.feedback}>Site Feedback</p>
+                <Link href="/homepage" passHref>
+                    <button className={styles.button}>
+                        About Us
+                    </button>
+                </Link>
+                <Link href="/homepage" passHref>
+                    <button className={styles.button}>
+                        FAQ
+                    </button>
+                </Link>
+                <Link href="/homepage" passHref>
+                    <button className={styles.button}>
+                        Contact Us
+                    </button>
+                </Link>
+                <Link href="/homepage" passHref>
+                    <button className={styles.button}>
+                        Site Feedback
+                    </button>
+                </Link>
             </div>
             <div className={styles.iconsDiv}>
                 <div className={styles.spacerForIcons}>
-                    <BsInstagram/>
+                    <Link href="/homepage" passHref>
+                        <button className={styles.button}>
+                            <BsInstagram/>
+                        </button>
+                    </Link>
                 </div>
                 <div className={styles.spacerForIcons}>
-                    <BsTwitter/>
+                    <Link href="/homepage" passHref>
+                        <button className={styles.button}>
+                            <BsTwitter/>
+                        </button>
+                    </Link>
                 </div>
                 <div className={styles.spacerForIcons}>
-                    <BsGithub/>
+                    <Link href="/homepage" passHref>
+                        <button className={styles.button}>
+                            <BsGithub/>
+                        </button>
+                    </Link>
                 </div>
                 <div className={styles.spacerForIcons}>
-                    <BsFacebook/>
+                    <Link href="/homepage" passHref>
+                        <button className={styles.button}>
+                            <BsFacebook/>
+                        </button>
+                    </Link>
                 </div>
             </div>
         </div>

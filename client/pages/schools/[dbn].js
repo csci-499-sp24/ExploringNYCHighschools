6857a1340b48c9ac8fd92cced82104663deafe89
orijinel <44485040+orijinel@users.noreply.github.com>
2024-04-08 02:58:12 -0400

    Update [dbn].js

--- client/pages/schools/[dbn].js
+++ client/pages/schools/[dbn].js
@@ -2,42 +2,55 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Card from "../../components/Card";
 import CardSquare from "../../components/CardSquare";
+import SchoolButton from "../../components/SchoolButton";
 
 function SchoolProfile() {
     const router = useRouter();
     const { dbn } = router.query;
     const [school, setSchool] = useState([]);
-    const[message, setMessage] = useState("Loading");
+    const [message, setMessage] = useState("Loading");
 
     useEffect(() => {
         if (dbn) {
             fetch(process.env.NEXT_PUBLIC_SERVER_URL + `/api/schools/${dbn}`)
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.school) {
-                    setSchool(data.school);
-                    setMessage(data.message);
-                } else {
-                    setMessage("School data not found!");
-                }
-            });
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.school) {
+                        setSchool(data.school);
+                        setMessage(data.message);
+                    } else {
+                        setMessage("School data not found!");
+                    }
+                });
         }
     }, [dbn]);
 
+    const handleGetDirections = (address) => {
+        router.push({
+            pathname: '/NYCMap',
+            query: { address },
+        });
+    };
+    
     return (
         <div className="background-color">
-        <section id="hero">
-            <div className="container">
-                <div className="row">
-                    <h1 className="display-1">{school.school_name}</h1>
-                    <p className="desc">{school.description}</p>
+            <section id="hero">
+                <div className="container">
+                    <div className="row">
+                        <h1 className="display-1">{school.school_name}</h1>
+                        <p className="desc">{school.description}</p>
+                    </div>
                 </div>
-            </div>
                 <div className="row">
                     <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
                         <div className="p-2 border flex-fill bd-highlight">
                             <Card text1={`Address: ${school.address}`} text2={`Website: ${school.website}`} text3={`Phone Number: ${school.phone_number}`} text4={`Email: ${school.email}`}></Card>
                         </div>
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-auto">
+                        <SchoolButton text={"Get Directions"} onClick={() => handleGetDirections(school.address)}></SchoolButton>
                     </div>
                 </div>
                 <div className="row">
@@ -51,7 +64,7 @@ function SchoolProfile() {
                         <div className="p-2 bd-highlight">
                             <CardSquare text1={"Graduation Rate"} text2={school.grad_rate} ></CardSquare>
                         </div>
-                    </div>     
+                    </div>
                 </div>
                 <div className="row">
                     <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
@@ -64,7 +77,7 @@ function SchoolProfile() {
                         <div className="p-2 bd-highlight">
                             <CardSquare text1={"Grade Span"} text2={school.grad_span} ></CardSquare>
                         </div>
-                    </div>     
+                    </div>
                 </div>
                 <div className="row">
                     <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
@@ -77,7 +90,7 @@ function SchoolProfile() {
                         <div className="p-2 bd-highlight">
                             <CardSquare text1={"PSAL Boys"} text2={school.psal_boys} ></CardSquare>
                         </div>
-                    </div>     
+                    </div>
                 </div>
                 <div className="row">
                     <div className="d-flex flex-row bd-highlight mb-3 justify-content-center">
@@ -90,14 +103,11 @@ function SchoolProfile() {
                         <div className="p-2 bd-highlight">
                             <CardSquare text1={"School Safety"} text2={school.student_safety} ></CardSquare>
                         </div>
-                    </div>     
+                    </div>
                 </div>
-        </section>
+            </section>
         </div>
     )
 }
 
 export default SchoolProfile;
-
-
-

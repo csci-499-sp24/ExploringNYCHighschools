2743ece51867333d750c2cb50fb6cfa81e7b66c6
Tammy Chiu <chiutammy182@gmail.com>
2024-04-09 20:53:22 -0400

    Bolded the question

--- client/components/Collapsible.js
+++ client/components/Collapsible.js
@@ -24,7 +24,7 @@ function Collapsible({question, school1_answer, school2_answer, expand, collapse
     return (
         <div className="collaspe-item">
             <div className="collaspe-title" onClick={handleArrowClicked}>
-                <div>{question}</div>
+                <div style={{ fontWeight: "bold"}}>{question}</div>
                 <div>{arrowClicked ? <SlArrowUp fill="black" />: <SlArrowDown fill="black"/>}</div>
             </div>
             {arrowClicked && (

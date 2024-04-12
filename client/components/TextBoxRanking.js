e49ef0daf4788247c73544b964323b038196fc26
Tammy Chiu <chiutammy182@gmail.com>
2024-04-11 21:43:15 -0400

    Added: Links to school profiles

--- client/components/TextBoxRanking.js
+++ client/components/TextBoxRanking.js
@@ -1,11 +1,15 @@
-function TextBoxRanking({name, content}) {
+function TextBoxRanking({name, content, school}) {
     if (content===null || content===undefined) {
         school1_answer = "Unavailable";
     }
+    const school_profile_link = `/schools/${school}`
     return (
         <div className="textbox-ranking-item">
             <div className="textbox-ranking-title"> 
-                <div style={{ fontSize: "18px"}}>{name}</div>
+                <div style={{ fontSize: "18px"}}>
+                    <a href={school_profile_link} style={{ color: "black", borderBottom:"1px solid transparent", textDecoration:"none"}} 
+                    onMouseOver={(e)=>{e.target.style.borderBottom= "1px solid black"}}>{name}</a>
+                </div>
             </div>
             <div className="textbox-ranking-body">
                     {` ${content}`}

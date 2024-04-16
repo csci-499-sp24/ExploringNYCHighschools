501509a29800817753acc184d104a03729a62aa7
Tammy Chiu <chiutammy182@gmail.com>
2024-04-16 17:40:22 -0400

    Fixed tests

--- server/data.test.js
+++ server/data.test.js
@@ -6,7 +6,8 @@ const QualityReports = require('./models/quality_reports');
 
 
 const express = require("express");
-const app = express();
+const app = require("./server.js");
+// const app = express();
 app.use(express.json());
 const cors = require('cors');
 app.use(cors());
@@ -104,9 +105,9 @@ test("Should fetch data from api call and create School and QualityReport models
 
 
 describe('GET /api/home', () => {
-   app.get("/api/home", (req, res) => {
-       res.json({message: "Hello World!"});
-   });
+//    app.get("/api/home", (req, res) => {
+//        res.json({message: "Hello World!"});
+//    });
    it('should respond with status 200 and message "Hello World!"', async () => {
      const response = await request(app).get('/api/home');
      expect(response.status).toBe(200); // Check if status code is 200
@@ -116,16 +117,16 @@ describe('GET /api/home', () => {
 
 
 describe('GET /api/quality-reports', () => {
-   app.get("/api/quality-reports", async (req, res) => {
-       try {
-           const reports = await QualityReports.findAll();
-           res.json({ reports });
+//    app.get("/api/quality-reports", async (req, res) => {
+//        try {
+//            const reports = await QualityReports.findAll();
+//            res.json({ reports });
       
-       } catch (err) {
-           console.error("Error in fetching school reports", err);
-           res.status(500).json({ error: "Internal server error" });
-       }
-   });
+//        } catch (err) {
+//            console.error("Error in fetching school reports", err);
+//            res.status(500).json({ error: "Internal server error" });
+//        }
+//    });
    it('should respond with status 200', async () => {
        const response = await request(app).get('/api/quality-reports');
        expect(response.status).toBe(200); // Check if status code is 200
@@ -133,153 +134,157 @@ describe('GET /api/quality-reports', () => {
 });
 
 describe('GET /api/schools', () => {
-   app.get("/api/schools", async (req, res) => {
-       try {
-       let filters = {};
-       if (req.query.languages) {
-           // Perform a wildcard search for language filter
-           filters.languages = { [Op.like]: `%${req.query.languages}%` };
-       }
-       if (req.query.neighborhood) {
-           // Perform a wildcard search for neighborhood filter
-           filters.neighborhood = { [Op.like]: `%${req.query.neighborhood}%` };
-       }
-       if (req.query.ap_classes && typeof req.query.ap_classes === "string") {
-           // Split the search terms
-           const apClasses = req.query.ap_classes.split(",");
-           // Construct an array of conditions for each search term using Op.substring
-           const apClassConditions = apClasses.map((cls) => ({
-           ap_classes: { [Op.substring]: cls.trim() },
-           }));
-           // Combine conditions using OR operator
-           if (!filters[Op.and]) filters[Op.and] = [];
-           filters[Op.and] = [...filters[Op.and], ...apClassConditions];
-       }
-       if (req.query.psal_boys && typeof req.query.psal_boys === "string") {
-           const psalBoysSports = req.query.psal_boys.split(",");
-           const psalBoysConditions = psalBoysSports.map((sport) => ({
-           psal_boys: { [Op.substring]: sport.trim() },
-           }));
-           if (!filters[Op.and]) filters[Op.and] = [];
-           filters[Op.and] = [...filters[Op.and], ...psalBoysConditions];
-       }
-       if (req.query.psal_girls && typeof req.query.psal_girls === "string") {
-           const psalGirlsSports = req.query.psal_girls.split(",");
-           const psalGirlsConditions = psalGirlsSports.map((sport) => ({
-           psal_girls: { [Op.substring]: sport.trim() },
-           }));
-           if (!filters[Op.and]) filters[Op.and] = [];
-           filters[Op.and] = [...filters[Op.and], ...psalGirlsConditions];
-       }
-       console.log(filters);
-       const schools = await School.findAll({
-           where: filters,
-       });
-       res.json({ schools });
-       } catch (err) {
-       console.error("Error in fetching schools data", err);
-       res.status(500).json({ error: "Internal server error" });
-       }
-   });
+//    app.get("/api/schools", async (req, res) => {
+//        try {
+//        let filters = {};
+//        if (req.query.languages) {
+//            // Perform a wildcard search for language filter
+//            filters.languages = { [Op.like]: `%${req.query.languages}%` };
+//        }
+//        if (req.query.neighborhood) {
+//            // Perform a wildcard search for neighborhood filter
+//            filters.neighborhood = { [Op.like]: `%${req.query.neighborhood}%` };
+//        }
+//        if (req.query.ap_classes && typeof req.query.ap_classes === "string") {
+//            // Split the search terms
+//            const apClasses = req.query.ap_classes.split(",");
+//            // Construct an array of conditions for each search term using Op.substring
+//            const apClassConditions = apClasses.map((cls) => ({
+//            ap_classes: { [Op.substring]: cls.trim() },
+//            }));
+//            // Combine conditions using OR operator
+//            if (!filters[Op.and]) filters[Op.and] = [];
+//            filters[Op.and] = [...filters[Op.and], ...apClassConditions];
+//        }
+//        if (req.query.psal_boys && typeof req.query.psal_boys === "string") {
+//            const psalBoysSports = req.query.psal_boys.split(",");
+//            const psalBoysConditions = psalBoysSports.map((sport) => ({
+//            psal_boys: { [Op.substring]: sport.trim() },
+//            }));
+//            if (!filters[Op.and]) filters[Op.and] = [];
+//            filters[Op.and] = [...filters[Op.and], ...psalBoysConditions];
+//        }
+//        if (req.query.psal_girls && typeof req.query.psal_girls === "string") {
+//            const psalGirlsSports = req.query.psal_girls.split(",");
+//            const psalGirlsConditions = psalGirlsSports.map((sport) => ({
+//            psal_girls: { [Op.substring]: sport.trim() },
+//            }));
+//            if (!filters[Op.and]) filters[Op.and] = [];
+//            filters[Op.and] = [...filters[Op.and], ...psalGirlsConditions];
+//        }
+//        console.log(filters);
+//        const schools = await School.findAll({
+//            where: filters,
+//        });
+//        res.json({ schools });
+//        } catch (err) {
+//        console.error("Error in fetching schools data", err);
+//        res.status(500).json({ error: "Internal server error" });
+//        }
+//    });
    it('should respond with status 200', async () => {
        const response = await request(app).get('/api/schools');
        expect(response.status).toBe(200); // Check if status code is 200
    });
 });
 describe('GET /api/schools/:dbn', () => {
-   app.get("/api/schools/:dbn", async (req, res) => {
-       const {dbn} = req.params;
-       try {
-           const school = await School.findOne({
-               where: {
-                   dbn: dbn // Use the dbn parameter to search for the school
-               }
-           });
-           if (!school) {
-               return res.status(404).json({ error: 'School not found!' });
-           }
-           res.status(200).json({school});
-       } catch (error) {
-               console.error('Error fetching school:', error);
-               return res.status(500).json({ error: 'Internal Server Error' });
-             }
-       }
-   )
-   });
+//    app.get("/api/schools/:dbn", async (req, res) => {
+//        const {dbn} = req.params;
+//        try {
+//            const school = await School.findOne({
+//                where: {
+//                    dbn: dbn // Use the dbn parameter to search for the school
+//                }
+//            });
+//            if (!school) {
+//                return res.status(404).json({ error: 'School not found!' });
+//            }
+//            res.status(200).json({school});
+//        } catch (error) {
+//                console.error('Error fetching school:', error);
+//                return res.status(500).json({ error: 'Internal Server Error' });
+//              }
+//        }
+//    )
+//    });
    it('should respond with status 200', async () => {
        const response = await request(app).get('/api/schools/01M292');
        expect(response.status).toBe(200); // Check if status code is 200
+    });
 });
 describe('GET /api/schools/:dbn', () => {
-   app.get("/api/schools/:dbn", async (req, res) => {
-       const {dbn} = req.params;
-       try {
-           const school = await School.findOne({
-               where: {
-                   dbn: dbn // Use the dbn parameter to search for the school
-               }
-           });
-           if (!school) {
-               return res.status(404).json({ error: 'School not found!' });
-           }
-           res.status(200).json({school});
-       } catch (error) {
-               console.error('Error fetching school:', error);
-               return res.status(500).json({ error: 'Internal Server Error' });
-             }
-       }
-   )
-   });
+//    app.get("/api/schools/:dbn", async (req, res) => {
+//        const {dbn} = req.params;
+//        try {
+//            const school = await School.findOne({
+//                where: {
+//                    dbn: dbn // Use the dbn parameter to search for the school
+//                }
+//            });
+//            if (!school) {
+//                return res.status(404).json({ error: 'School not found!' });
+//            }
+//            res.status(200).json({school});
+//        } catch (error) {
+//                console.error('Error fetching school:', error);
+//                return res.status(500).json({ error: 'Internal Server Error' });
+//              }
+//        }
+//    )
+//    });
    it('should respond with status 404', async () => {
        const response = await request(app).get('/api/schools/error');
        expect(response.status).toBe(404); // Check if status code is 404
+    });
 });
 describe('GET /api/schools/quality-reports/:dbn', () => {
-   app.get("/api/schools/quality-reports/:dbn", async (req, res) => {
-       const {dbn} = req.params;
-       try {
-           const school = await QualityReports.findOne({
-               where: {
-                   dbn: dbn // Use the dbn parameter to search for the school
-               }
-           });
-           if (!school) {
-               return res.status(404).json({ error: 'School not found!' });
-           }
-           res.status(200).json({school});
-       } catch (error) {
-               console.error('Error fetching school:', error);
-               return res.status(500).json({ error: 'Internal Server Error' });
-           }
-       }
-   )
-   });
+//    app.get("/api/schools/quality-reports/:dbn", async (req, res) => {
+//        const {dbn} = req.params;
+//        try {
+//            const school = await QualityReports.findOne({
+//                where: {
+//                    dbn: dbn // Use the dbn parameter to search for the school
+//                }
+//            });
+//            if (!school) {
+//                return res.status(404).json({ error: 'School not found!' });
+//            }
+//            res.status(200).json({school});
+//        } catch (error) {
+//                console.error('Error fetching school:', error);
+//                return res.status(500).json({ error: 'Internal Server Error' });
+//            }
+//        }
+//    )
+//    });
    it('should respond with status 200', async () => {
        const response = await request(app).get('/api/schools/quality-reports/01M292');
        expect(response.status).toBe(200); // Check if status code is 200
+    });
 });
 describe('GET /api/schools/quality-reports/:dbn', () => {
-   app.get("/api/schools/quality-reports/:dbn", async (req, res) => {
-       const {dbn} = req.params;
-       try {
-           const school = await QualityReports.findOne({
-               where: {
-                   dbn: dbn // Use the dbn parameter to search for the school
-               }
-           });
-           if (!school) {
-               return res.status(404).json({ error: 'School not found!' });
-           }
-           res.status(200).json({school});
-       } catch (error) {
-               console.error('Error fetching school:', error);
-               return res.status(500).json({ error: 'Internal Server Error' });
-           }
-       }
-   )
-   });
+//    app.get("/api/schools/quality-reports/:dbn", async (req, res) => {
+//        const {dbn} = req.params;
+//        try {
+//            const school = await QualityReports.findOne({
+//                where: {
+//                    dbn: dbn // Use the dbn parameter to search for the school
+//                }
+//            });
+//            if (!school) {
+//                return res.status(404).json({ error: 'School not found!' });
+//            }
+//            res.status(200).json({school});
+//        } catch (error) {
+//                console.error('Error fetching school:', error);
+//                return res.status(500).json({ error: 'Internal Server Error' });
+//            }
+//        }
+//    )
+//    });
    it('should respond with status 404', async () => {
        const response = await request(app).get('/api/schools/quality-reports/error');
        expect(response.status).toBe(404); // Check if status code is 404
+    });
 });
 

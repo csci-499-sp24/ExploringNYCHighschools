297e4efdb9d3e4b24599ae4f8cb7b21d1740699f
presence18 <pikafan9@gmail.com>
2024-04-18 15:12:17 -0400

    added picture transitions to just the homepage

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "289d8f66c83493c2"; }
+/******/ 		__webpack_require__.h = function() { return "13828043fac8fb38"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

65b8da1c6a9d0e59def72b991d7c66e71cb76c8a
Tammy Chiu <chiutammy182@gmail.com>
2024-04-19 17:31:22 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ const { Op } = require("sequelize");
 const syncDB = async () => {
     try {
       // await db.sync({force: true}) // drop table if already exists
-      await db.sync() // not using force: true here since all data is already saved in database(no need to repopulate every time you run the server)
         console.log("Database connected");
         // await fetchData();        // puts data into database(commented out bc all data is already saved in database)
         console.log("Added data from hs directory");

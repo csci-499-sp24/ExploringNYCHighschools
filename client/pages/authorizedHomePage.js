6a71aa55bb31767501362815d378bf682622e134
orijinel <osingchen@gmail.com>
2024-05-15 14:36:34 -0400

    changed picture for homepage and authorized homepage

--- client/pages/authorizedHomePage.js
+++ client/pages/authorizedHomePage.js
@@ -38,7 +38,7 @@ const LoggedinHomepage = () => {
               </p>
             </div>
             <div style={{ flex: "1", paddingLeft: "20px" }}>
-              <img src="./mother-and-daughter.jpg" alt="mother and daughter together" style={{ maxWidth: "80%", height: "auto" }} />
+              <img src="./doe.jpg" alt="doe" style={{ maxWidth: "80%", height: "auto" }} />
             </div>
           </div>
         </div>
